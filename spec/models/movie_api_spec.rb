f692df8dcb04f7f315492f91e3752798f316bba8
Zach Stearns <zach.stearns92@gmail.com>
2020-12-31 01:04:21 -0700

    Test files in switched places; corrected

--- spec/models/movie_api_spec.rb
+++ spec/models/movie_api_spec.rb
@@ -1,26 +1,12 @@
 require 'rails_helper'
 
-describe MovieApiService do
-  context "class methods" do
-    it ".conn" do
-      VCR.use_cassette('top_movies') do
-        api_response = MovieApiService.conn.get("/3/movie/top_rated")
-        expect(api_response.status).to eq(200)
-      end
-    end
+describe Movie, type: :model do
+  describe "relationships" do
+    it { should have_many :parties }
+  end
 
-    it ".parse_data" do
-      VCR.use_cassette('top_movies') do
-        api_response = MovieApiService.conn.get("/3/movie/top_rated")
-        expect(MovieApiService.parse_data(api_response).class).to eq(Hash)
-      end
-    end
-
-    it ".top_rated_movies" do
-      VCR.use_cassette('top_rated_movies') do
-        expect(MovieApiService.top_rated_movies.class).to eq(Array)
-        expect(MovieApiService.top_rated_movies.count).to eq(40)
-      end
-    end
+  describe "validations" do
+    it { should validate_presence_of :api_id }
+    it { should validate_presence_of :title }
   end
 end
